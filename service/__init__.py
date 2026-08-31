ebdf1046eeda62acc19e06a5e2a064222044eb39
Ben1072 <pjmafojane@gmail.com>
2026-08-31 11:52:31 +0200

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -8,10 +8,18 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
+from service import talisman
+from flask_cors import CORS
+
+CORS(app)
+
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+
+talisman = Talisman(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
@@ -35,3 +43,17 @@ except Exception as error:  # pylint: disable=broad-except
     sys.exit(4)
 
 app.logger.info("Service initialized!")
+
+@classmethod
+def setUpClass(cls):
+    """Run once before all tests"""
+    { other lines of code here ... }
+    talisman.force_https = False
+
+    def test_cors_security(self):
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+
